1c7d622c065ad1d6fb1d20174c0c9745d51a8da2
HannahPaalisbo <102515641+HannahPaalisbo@users.noreply.github.com>
2023-09-28 17:58:32 +0800

    Create getTeacher.php

---
+++ getTeacher.php
@@ -0,0 +1,37 @@
+<?php
+header('Content-Type: application/json');
+
+require 'connection.php';
+
+try {
+    $data = array();
+    
+    $tableName = "tbl_teacher";
+    $query = "SELECT * FROM " . $tableName;
+    $result = mysqli_query($db_con, $query);
+
+    teacherId = "";
+    $firstName = "";
+    $lastName = "";
+
+    $item = array(
+        "teacherId" => teacherId,
+        "firstName" => $firstName,
+        "lastName" => $lastName
+    );
+
+    while($row = mysqli_fetch_assoc($result)) {
+        $item["teacherId"] = $row['teacher_id'];
+        $item["firstName"] = $row['firstName'];
+        $item["lastName"] = $row['lastName'];
+        $data[] = $item;
+    }                     
+
+    $jsonData = json_encode($data);
+    echo $jsonData;
+} catch (Exception $e) {
+    $response = "$e";
+    echo json_encode($response);
+}
+mysqli_close($db_con)
+?>
