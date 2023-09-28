49bd8b3b7224a21446e82b4bf99b79e039f5004f
HannahPaalisbo <102515641+HannahPaalisbo@users.noreply.github.com>
2023-09-28 17:48:05 +0800

    Create getLanguage.php

---
+++ getLanguage.php
@@ -0,0 +1,42 @@
+<?php
+header('Content-Type: application/json');
+
+require 'connection.php';
+
+try {
+    $data = array();
+    
+    $tableName = "tbl_language";
+    $query = "SELECT * FROM " . $tableName;
+    $result = mysqli_query($db_con, $query);
+
+    $languageId = "";
+    $kalagan = "";
+    $filipino = "";
+    $english = "";
+    $langTopicRef = "";
+
+    $item = array(
+        "languageId" => $languageId,
+        "kalagan" => $kalagan,
+        "filipino" => $filipino,
+        "english" => $english,
+        "langTopicRef" => $langTopicRef
+    );
+
+    while($row = mysqli_fetch_assoc($result)) {
+        $item["kalagan"] = $row['kalagan'];
+        $item["filipino"] = $row['filipino'];
+        $item["english"] = $row['english'];
+        $item["langTopicRef"] = $row['topic_id'];
+        $data[] = $item;
+    }                     
+
+    $jsonData = json_encode($data);
+    echo $jsonData;
+} catch (Exception $e) {
+    $response = "$e";
+    echo json_encode($response);
+}
+mysqli_close($db_con)
+?>
